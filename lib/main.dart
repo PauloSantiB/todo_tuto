f5a9fe71016adae13544ef66247f27bc2747633b
Paulo Benitez <paulosantibd@gmail.com>
2025-01-23 21:55:59 -0300

    todobloc

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_tuto/home.dart';
+import 'package:todo_tuto/todo_bloc/todo_bloc.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+     debugShowCheckedModeBanner: false,
+     title: 'Todo App',
+     theme: ThemeData(
+      colorScheme: const ColorScheme.light(
+        background: Colors.white,
+        onBackground: Colors.black,
+        primary: Colors.pink,
+        onPrimary: Colors.black,
+        secondary: Colors.pinkAccent,
+        onSecondary: Colors.white
+      )
+     ),
+     home: BlocProvider<TodoBloc>(
+      create: (context) => TodoBloc()..add(
+        TodoStarted()
+      ),
+    child:const HomeScreen(),
+
+     )
+
+    );
+  }
+}
